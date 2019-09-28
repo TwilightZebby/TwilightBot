42ee6077b234e655b66c531d1cf5f95b1ad118e1
Zebby_ <48347420+TwilightZebby@users.noreply.github.com>
2019-09-28 07:53:59 +0100

    Made rps cmd less pointless
    AKA I actually made it functional! :P

--- commands/rps.js
+++ commands/rps.js
@@ -1,3 +1,5 @@
+const Discord = require("discord.js");
+
 module.exports = {
     name: 'rps',
     description: 'Play Rock Paper Scissors with the Bot!',
@@ -7,30 +9,84 @@ module.exports = {
     args: true,
     guildAccess: 'all',
     execute(message, args) {
+      // Embed time!
+      // Embed colour will be set dependant on who wins.
+      // Basically: Green = User won. Red = User lose. Pink = Draw!
+      // Green: #03fc24
+      // Red: #fc0303
+      // Pink?: #ae00ff
+      const rpsEmbed = new Discord.MessageEmbed().setFooter('Rock Paper Scissors Game');
+
+      // To randomise the Bot's choice
+      const rpsArray = ['rock', 'paper', 'scissors'];
+      const intRpsChoice = Math.floor((Math.random() * 3) + 0);
+      const rpsChoice = rpsArray[intRpsChoice];
+
       // Grab User input
-      const usersChoice = args.shift();
+      const usersChoice = args.shift().toLowerCase();
+      const userObject = message.author;
 
-      // ROCK
-      if(usersChoice === 'rock' || usersChoice === 'r') {
-        message.channel.send(`**Rock!**`);
-        return message.channel.send(`It ended in a draw!`);
+      // User = rock; Bot = rock
+      if(usersChoice === 'rock' || usersChoice === 'r' && rpsChoice === 'rock') {
+        rpsEmbed.setColor('#ae00ff');
+        rpsEmbed.addField(`Rock!`, `Well.....seems like it was a draw.`);
+        return message.channel.send(rpsEmbed);
       }
-      // PAPER
-      else if(usersChoice === 'paper' || usersChoice === 'p') {
-        message.channel.send(`**Rock!**`);
-        return message.channel.send(`${message.author} won!`);
+      // User = rock; Bot = Paper
+      else if(usersChoice === 'rock' || usersChoice === 'r' && rpsChoice === 'paper') {
+        rpsEmbed.setColor('#fc0303');
+        rpsEmbed.addField(`Paper!`, `Tough luck. ${userObject} lost!`);
+        return message.channel.send(rpsEmbed);
       }
-      // SCISSORS
-      else if(usersChoice === 'scissors' || usersChoice === 's') {
-        message.channel.send(`**Rock!**`);
-        return message.channel.send(`Shame, ${message.author} lost...`);
+      // User = rock; Bot = scissors
+      else if(usersChoice === 'rock' || usersChoice === 'r' && rpsChoice === 'scissors') {
+        rpsEmbed.setColor('#03fc24');
+        rpsEmbed.addField(`Scissors!`, `Welp, ${userObject} won!`);
+        return message.channel.send(rpsEmbed);
       }
-      // Invaild Input
+      // User = paper; Bot = rock
+      else if(usersChoice === 'paper' || usersChoice === 'p' && rpsChoice === 'rock') {
+        rpsEmbed.setColor('#03fc24');
+        rpsEmbed.addField(`Rock!`, `Welp, ${userObject} won!`);
+        return message.channel.send(rpsEmbed);
+      }
+      // User = paper; Bot = Paper
+      else if(usersChoice === 'paper' || usersChoice === 'p' && rpsChoice === 'paper') {
+        rpsEmbed.setColor('#ae00ff');
+        rpsEmbed.addField(`Paper!`, `Well.....seems like it was a draw.`);
+        return message.channel.send(rpsEmbed);
+      }
+      // User = paper; Bot = scissors
+      else if(usersChoice === 'paper' || usersChoice === 'p' && rpsChoice === 'scissors') {
+        rpsEmbed.setColor('#fc0303');
+        rpsEmbed.addField(`Scissors!`, `Tough luck. ${userObject} lost!`);
+        return message.channel.send(rpsEmbed);
+      }
+      // User = scissors; Bot = rock
+      else if(usersChoice === 'scissors' || usersChoice === 's' && rpsChoice === 'rock') {
+        rpsEmbed.setColor('#fc0303');
+        rpsEmbed.addField(`Rock!`, `Tough luck. ${userObject} lost!`);
+        return message.channel.send(rpsEmbed);
+      }
+      // User = scissors; Bot = Paper
+      else if(usersChoice === 'scissors' || usersChoice === 's' && rpsChoice === 'paper') {
+        rpsEmbed.setColor('#03fc24');
+        rpsEmbed.addField(`Paper!`, `Welp, ${userObject} won!`);
+        return message.channel.send(rpsEmbed);
+      }
+      // User = scissors; Bot = scissors
+      else if(usersChoice === 'scissors' || usersChoice === 's' && rpsChoice === 'scissors') {
+        rpsEmbed.setColor('#ae00ff');
+        rpsEmbed.addField(`Scissors!`, `Well.....seems like it was a draw.`);
+        return message.channel.send(rpsEmbed);
+      }
+      // Otherwise
       else {
-        message.channel.send(`**Roc**-- wait, that\'s neither Rock, Paper, nor Scissors....`);
-        return message.channel.send(`${message.author} loses by default!`);
+        rpsEmbed.setColor('#ae00ff');
+        rpsEmbed.addField(`Unexpected Input`, `You can only use "rock", "paper", "scissors", "r", "p", or "s".\nPlease try again using one of these inputs.`);
+        return message.channel.send(rpsEmbed);
       }
 
-      //END OF COMMAND
+      // END OF COMMAND
     },
 };
